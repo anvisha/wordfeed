4f125f574889cc86b4e47357392e417384259dc6
D <dyim@mit.edu>
2013-07-27 16:33:03 -0700

    Added skeleton foursquare_json_handler.py and sample Foursquare JSON

---
+++ translate_logic/foursquare_json_handler.py
@@ -0,0 +1,7 @@
+# Handles Foursquare JSON. Takes in a JSON string and returns a dictionary mapping
+# relevant categories to their values
+import json # build-in Python library
+
+def handler(jsonString):
+    # http://docs.python.org/2/library/json.html
+    # handy resource
