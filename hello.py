634d68de9ba39cde88da906eb277334749e709b4
anvisha <anvisha@gmail.com>
2013-07-27 15:19:14 -0700

    hi user

--- hello.py
+++ hello.py
@@ -6,3 +6,8 @@ app = Flask(__name__)
 @app.route('/hello')
 def hello():
     return "Hi! Let's learn new languages on the go!"
+
+@app.route('/user/<username>')
+def user_hi(username):
+    return "hi "+ username
+
