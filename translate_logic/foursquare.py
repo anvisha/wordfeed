118c469438fbcea1f296c2cb413eaa882bf7f1bd
anvisha <anvisha@gmail.com>
2013-07-28 05:21:34 -0700

    almost there

--- translate_logic/foursquare.py
+++ translate_logic/foursquare.py
@@ -43,12 +43,11 @@ def get_fields(id):
 def parse_foursquare_push(j):
     response = json.loads(j)
     user_id = response['user']['id']
-    #categories = response['venue']['categories']
-    #catList = [x['name'] for x in categories]
-    #fieldDict['categories'] = catList
-    #fieldDict['name'] = response['venue']['name']
-    #return fieldDict, user_id
-    return user_id
+    categories = response['venue']['categories']
+    catList = [x['name'] for x in categories]
+    fieldDict['categories'] = catList
+    fieldDict['name'] = response['venue']['name']
+    return fieldDict, user_id
     
 # Used for hello.py: ID demo
 def get_words_from_id(id):
